636f30d9ed5ee3c5c580dac9b6d48a5a8d915de4
Brian <bmfenno@gmail.com>
2020-03-12 17:06:37 -0400

    First

---
+++ build/CMakeFiles/membot.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/membot.dir/src/chatbot.o"
+  "CMakeFiles/membot.dir/src/chatgui.o"
+  "CMakeFiles/membot.dir/src/chatlogic.o"
+  "CMakeFiles/membot.dir/src/graphedge.o"
+  "CMakeFiles/membot.dir/src/graphnode.o"
+  "membot.pdb"
+  "membot"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/membot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
